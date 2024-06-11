eada0e464e69f21828edbfa5f13467d894cc85e1
dyziob <dyziob65@gmail.com>
2024-06-11 21:34:37 +0200

    Adding comments

--- app/src/main/java/com/example/projekt/QuizSummaryActivity.java
+++ app/src/main/java/com/example/projekt/QuizSummaryActivity.java
@@ -19,43 +19,54 @@ import androidx.appcompat.widget.AppCompatButton;
 import java.util.Random;
 
 public class QuizSummaryActivity extends AppCompatActivity {
+    // Deklaracja OnBackPressedDispatcher
     private final OnBackPressedDispatcher onBackPressedDispatcher = getOnBackPressedDispatcher();
+    // Deklaracja SharedPreferences
     private SharedPreferences sharedPreferences;
 
+    // Deklaracja AppCompatButton
     private AppCompatButton appCompatButton;
 
     @SuppressLint("StringFormatInvalid")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Ukrycie action bar
         getSupportActionBar().hide();
         setContentView(R.layout.activity_quiz_summary);
 
+        // Inicjalizacja przycisku powrotu do głównego menu
         appCompatButton = findViewById(R.id.backToMainBtn);
 
+        // Inicjalizacja SharedPreferences
         sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
         boolean nightMODE = sharedPreferences.getBoolean("night", false);
 
+        // Ustawienie trybu nocnego
         if (nightMODE) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
 
+        // Inicjalizacja widoków
         final ImageView backBtn = findViewById(R.id.backBtn);
         final TextView correctAnswersCircle = findViewById(R.id.correctAnswersCircle);
         final TextView correctAnswers = findViewById(R.id.correctAnswers);
         final TextView incorrectAnswers = findViewById(R.id.incorrectAnswers);
         final LinearLayout randomPlayerResults = findViewById(R.id.randomPlayerResults);
 
+        // Pobranie danych z Intent
         Intent intent = getIntent();
         int correct = intent.getIntExtra(getString(R.string.correct), 0);
         int incorrect = intent.getIntExtra(getString(R.string.incorrect), 0);
 
+        // Ustawienie tekstów dla widoków
         correctAnswersCircle.setText(String.valueOf(correct));
         correctAnswers.setText(String.format(getString(R.string.correct_answers), correct));
         incorrectAnswers.setText(String.format(getString(R.string.incorrect_answers), incorrect));
 
+        // Generowanie losowych wyników graczy
         Random random = new Random();
         int numberOfPlayers = 5;
         String[] playerNames = {"Alice", "Bob", "Charlie", "Dave", "Eve"};
@@ -65,21 +76,23 @@ public class QuizSummaryActivity extends AppCompatActivity {
             int randomIncorrect = 5 - randomCorrect;
             String playerName = playerNames[random.nextInt(playerNames.length)];
 
+            // Tworzenie nowego TextView dla wyniku gracza
             TextView playerResultView = new TextView(this);
             playerResultView.setText(String.format(getString(R.string.player_result), playerName, randomCorrect, randomIncorrect));
             playerResultView.setTextSize(12);
             playerResultView.setPadding(10, 10, 10, 10);
-            //playerResultView.setTextColor(getResources().getColor(R.color.black));
             playerResultView.setTextAppearance(this, R.style.Text);
 
+            // Ustawienie parametrów layoutu
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                     0, LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f);
             playerResultView.setLayoutParams(params);
 
+            // Dodanie TextView do LinearLayout
             randomPlayerResults.addView(playerResultView);
         }
 
-        // Back button
+        // Obsługa kliknięcia przycisku powrotu
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,7 +101,7 @@ public class QuizSummaryActivity extends AppCompatActivity {
             }
         });
 
-        // OnBackPressed
+        // Obsługa zdarzenia naciśnięcia przycisku wstecz
         onBackPressedDispatcher.addCallback(new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
@@ -97,6 +110,7 @@ public class QuizSummaryActivity extends AppCompatActivity {
             }
         });
 
+        // Obsługa kliknięcia przycisku powrotu do głównego menu
         appCompatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/projekt/Repository/Login.java
+++ app/src/main/java/com/example/projekt/Repository/Login.java
@@ -26,83 +26,105 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
-    public TextInputEditText editTextEmail;
-    public TextInputEditText editTextPassword;
-    public Button buttonLog;
-    public FirebaseAuth mAuth;
+    // Elementy interfejsu użytkownika
+    TextInputEditText editTextEmail, editTextPassword;
+    Button buttonLog;
     ProgressBar progressBar;
     TextView textView;
 
-    SharedPreferences sharedPreferences;
+    // Firebase Authentication
+    FirebaseAuth mAuth;
 
+    // SharedPreferences do przechowywania ustawień aplikacji
+    SharedPreferences sharedPreferences;
 
     @Override
     public void onStart() {
         super.onStart();
+        // Sprawdzenie, czy użytkownik jest już zalogowany
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
+        if (currentUser != null) {
+            // Jeśli zalogowany, przekierowanie do MainActivity
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
             finish();
         }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Inicjalizacja Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+
+        // Powiązanie elementów interfejsu użytkownika
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.password);
         buttonLog = findViewById(R.id.loginbtn);
         progressBar = findViewById(R.id.progressBar);
         textView = findViewById(R.id.register_now);
+
+        // Wczytanie preferencji trybu nocnego
         sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
         boolean nightMODE = sharedPreferences.getBoolean("night", false);
 
+        // Ustawienie trybu nocnego na podstawie preferencji
         if (nightMODE) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
 
+        // Ustawienie listenera dla tekstu "Zarejestruj się teraz"
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Przekierowanie do aktywności rejestracji
                 Intent intent = new Intent(getApplicationContext(), Registration.class);
                 startActivity(intent);
                 finish();
             }
         });
 
+        // Ustawienie listenera dla przycisku logowania
         buttonLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                progressBar.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.VISIBLE); // Pokazanie paska postępu
+
+                // Pobranie wprowadzonych danych email i hasła
                 String email, password;
                 email = String.valueOf(editTextEmail.getText());
                 password = String.valueOf(editTextPassword.getText());
 
-                if (TextUtils.isEmpty(email)){
+                // Walidacja email
+                if (TextUtils.isEmpty(email)) {
                     Toast.makeText(Login.this, R.string.Empty_Email, Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                if (TextUtils.isEmpty(password)){
+                // Walidacja hasła
+                if (TextUtils.isEmpty(password)) {
                     Toast.makeText(Login.this, R.string.password_empty, Toast.LENGTH_SHORT).show();
                     return;
                 }
 
+                // Próba zalogowania się za pomocą email i hasła
                 mAuth.signInWithEmailAndPassword(email, password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                progressBar.setVisibility(View.GONE);
+                                progressBar.setVisibility(View.GONE); // Ukrycie paska postępu
                                 if (task.isSuccessful()) {
+                                    // Sukces logowania, wyświetlenie komunikatu i przekierowanie do MainActivity
                                     Toast.makeText(Login.this, R.string.login_successful, Toast.LENGTH_SHORT).show();
                                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                                     startActivity(intent);
                                     finish();
                                 } else {
+                                    // W przypadku niepowodzenia logowania, wyświetlenie komunikatu
                                     Toast.makeText(Login.this, R.string.authentication_failed, Toast.LENGTH_SHORT).show();
                                 }
                             }
